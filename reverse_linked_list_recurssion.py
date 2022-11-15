17e1dfca0b084824aa80466610008f72c9ba274d
Yaswanth Mareedu <yaswanthyashu333@gmail.com>
2022-11-15 01:25:20 -0500

    Reverse a Linked List : Recurssion

---
+++ reverse_linked_list_recurssion.py
@@ -0,0 +1,12 @@
+# Definition for singly-linked list.
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head==None or head.next==None:
+            return head
+        ans=self.reverseList(head.next)
+        q=head.next
+        q.next=head
+        head.next=None
+        return ans  
+        
+
