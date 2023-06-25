4688a678ad2a54ec2f0283832e6aa7d4afd0ddb5
YaswanthMareedu <45549385+YaswanthMareedu@users.noreply.github.com>
2023-06-24 23:13:01 -0400

    Update climbing_stairs.py
    Recursive Solution

--- climbing_stairs.py
+++ climbing_stairs.py
@@ -1,1 +1,8 @@
-
+class Solution:
+    def result(self,n):
+        if n==0 or n==1:
+            return 1
+        return self.result(n-1)+self.result(n-2)
+    def climbStairs(self, n: int) -> int:
+        return self.result(n)
+        
