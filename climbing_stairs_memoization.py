aa014c4360886777c05e27c9747204fbc5153b7f
YaswanthMareedu <45549385+YaswanthMareedu@users.noreply.github.com>
2023-06-24 23:22:06 -0400

    Update climbing_stairs_memoization.py

--- climbing_stairs_memoization.py
+++ climbing_stairs_memoization.py
@@ -1,1 +1,12 @@
-
+class Solution:
+    def result(self,n,t):
+        if t[n]!=-1:
+            return t[n]
+        if n==0 or n==1:
+            return 1
+        t[n] = self.result(n-1,t)+self.result(n-2,t)
+        #print(t,n)
+        return t[n]
+    def climbStairs(self, n: int) -> int:
+        t = [-1 for i in range(n+1)]
+        return self.result(n,t)
