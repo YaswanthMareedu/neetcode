885fa1e835e11b9bbb94bc4d31bdc5df9052eb40
YaswanthMareedu <45549385+YaswanthMareedu@users.noreply.github.com>
2023-06-24 23:31:03 -0400

    Update climbing_stairs_top_down.py

--- climbing_stairs_top_down.py
+++ climbing_stairs_top_down.py
@@ -1,1 +1,12 @@
-
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        t = [0 for i in range(n+1)]
+        #return self.result(n,t)
+        for i in range(n+1):
+            if i==0 or i==1:
+                t[i]=1
+            if i>=2:
+                t[i]=t[i-1]+t[i-2]
+        #print(t)
+        return t[n]
+        
