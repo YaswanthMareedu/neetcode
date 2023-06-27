34e66917822a9e69d30d08d97e522d00ecfb163c
YaswanthMareedu <45549385+YaswanthMareedu@users.noreply.github.com>
2023-06-26 23:54:01 -0400

    Update coin_change_2.py

--- coin_change_2.py
+++ coin_change_2.py
@@ -1,7 +1,24 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
+        #Method 1 (recursive but we get time limit exceeded)
+        t = [-1 for i in range(len(coins))]
+        n = len(coins)
+        def findways(coins,s,n):
+            if s<0:
+                return 0
+            if s==0:
+                return 1
+            if n<0:
+                return 0
+
+            return findways(coins,s-coins[n],n)+findways(coins,s,n-1)
+        
+        #return findways(coins,amount,n-1)
+
+        #Recursion with memoization (Solved)
         t = {}
         n = len(coins)
+        s = amount
         def findways(coins,s,n):
             if s<0:
                 return 0
@@ -14,5 +31,4 @@ class Solution:
 
             t[(s,n)]=findways(coins,s-coins[n-1],n)+findways(coins,s,n-1)
             return t[(s,n)]
-        return findways(coins,amount,n)
-
+        #return findways(coins,amount,n)
