0a0804ee989074f3b40a8d96a8a3682290a9916e
YaswanthMareedu <45549385+YaswanthMareedu@users.noreply.github.com>
2023-06-26 23:52:05 -0400

    Update coin_change_2.py

--- coin_change_2.py
+++ coin_change_2.py
@@ -1,1 +1,18 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        t = {}
+        n = len(coins)
+        def findways(coins,s,n):
+            if s<0:
+                return 0
+            if s==0:
+                return 1
+            if n<=0:
+                return 0
+            if (s,n) in t:
+                return t[(s,n)]
+
+            t[(s,n)]=findways(coins,s-coins[n-1],n)+findways(coins,s,n-1)
+            return t[(s,n)]
+        return findways(coins,amount,n)
 
