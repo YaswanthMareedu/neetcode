e9d133331e864c9a69581ed7247761447eef8b41
YaswanthMareedu <45549385+YaswanthMareedu@users.noreply.github.com>
2023-06-27 23:48:10 -0400

    Update coin_change2_dp.py

--- coin_change2_dp.py
+++ coin_change2_dp.py
@@ -3,19 +3,6 @@ class Solution:
         t = {}
         n = len(coins)
         s = amount
-        def findways(coins,s,n):
-            if s<0:
-                return 0
-            if s==0:
-                return 1
-            if n<=0:
-                return 0
-            if (s,n) in t:
-                return t[(s,n)]
-
-            t[(s,n)]=findways(coins,s-coins[n-1],n)+findways(coins,s,n-1)
-            return t[(s,n)]
-        #return findways(coins,amount,n)
 
         t = [[0 for i in range(s+1)] for j in range(n+1)]
         #print(len(t),len(t[0]),t)
