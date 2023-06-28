87258dd4d763c9cde76f33854ceea1eaef2a4ada
YaswanthMareedu <45549385+YaswanthMareedu@users.noreply.github.com>
2023-06-27 23:51:11 -0400

    Update coin_change_2.py
    Added Dp Top down approach

--- coin_change_2.py
+++ coin_change_2.py
@@ -32,3 +32,24 @@ class Solution:
             t[(s,n)]=findways(coins,s-coins[n-1],n)+findways(coins,s,n-1)
             return t[(s,n)]
         #return findways(coins,amount,n)
+
+        #Method3 with DP (Top - down)
+        n = len(coins)
+        s = amount
+        t = [[0 for i in range(s+1)] for j in range(n+1)]
+        #print(len(t),len(t[0]),t)
+
+        for i in range(n+1):
+            for j in range(amount+1):
+                #print(i,j,amount)
+                if j==0:
+                    t[i][j]=1
+                elif i==0:
+                    t[i][j]=0
+                else:
+                    if j>=coins[i-1]:
+                        t[i][j]=t[i][j-coins[i-1]]+t[i-1][j]
+                    else:
+                        t[i][j]=t[i-1][j]
+        #print(t)
+        return t[n][amount]
