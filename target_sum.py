dd17c9df32a771c8b813b7bead3fa2d3afc2c00b
YaswanthMareedu <45549385+YaswanthMareedu@users.noreply.github.com>
2023-06-30 23:30:13 -0400

    Update target_sum.py

--- target_sum.py
+++ target_sum.py
@@ -1,1 +1,23 @@
+import math
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        s = math.ceil((sum(nums)+target)/2)
 
+        n = len(nums)
+        #print(s)
+        #t = [[0 for j in range(s+1)] for i in range(len(nums)+1)]
+        t= {}
+        coins = nums
+        s = target
+        def findways(coins,s,n,target):
+            if n<=0:
+                if s==target:
+                    return 1
+                else:
+                    return 0
+            if (s,n) in t:
+                return t[(s,n)]
+
+            t[(s,n)]=findways(coins,s-coins[n-1],n-1,target)+findways(coins,s+coins[n-1],n-1,target)
+            return t[(s,n)]
+        return findways(coins,0,n,target)
